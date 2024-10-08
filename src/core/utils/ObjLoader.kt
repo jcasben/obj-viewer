3a40f96cf2aa876a3a73399ba0a71ba87d509db8
jcasben <jcasben02@gmail.com>
2024-10-08 21:06:28 +0200

    [FEAT]: implemented obj loader and viewer

--- src/core/utils/ObjLoader.kt
+++ src/core/utils/ObjLoader.kt
@@ -1,5 +1,32 @@
 package core.utils
 
-class ObjLoader {
+import core.structures.IndexedFace
+import core.structures.Vec4
+import java.io.File
 
+class ObjLoader(private val file: String) {
+
+    fun parseObj(): IndexedFace {
+        val vertexes = mutableListOf<Vec4>()
+        val indexes = mutableListOf<Int>()
+
+        File(file).forEachLine { line ->
+            val parts = line.trim().split("\\s+".toRegex())
+            when (parts[0]) {
+                "v" -> {
+                    if (parts.size >= 4) {
+                        vertexes.add(
+                            Vec4(
+                                x = parts[1].toFloat(),
+                                y = parts[2].toFloat()
+                            )
+                        )
+                    }
+                }
+                "f" -> indexes.addAll(parts.drop(1).map { it.split("\\s+".toRegex())[0].toInt() - 1 })
+            }
+        }
+
+        return IndexedFace(vertexes, indexes)
+    }
 }
--- src/ui/ObjectViewer.kt
+++ src/ui/ObjectViewer.kt
@@ -1,4 +1,50 @@
 package ui
 
-class ObjectViewer {
+import core.structures.IndexedFace
+import core.utils.ObjLoader
+import java.awt.Color
+import java.awt.Graphics
+import javax.swing.JPanel
+
+class ObjectViewer : JPanel() {
+
+    private var objLoader: ObjLoader = ObjLoader("icosphere.obj")
+
+    override fun paintComponent(g: Graphics) {
+        super.paintComponent(g)
+        g.color = Color.BLACK
+
+        drawObject(objLoader.parseObj(), g)
+    }
+
+    private fun drawObject(obj: IndexedFace, g: Graphics) {
+        for (i in obj.indexes.indices step 3) {
+            if (i + 2 < obj.indexes.size) {
+                val vertex1 = obj.vertexes[obj.indexes[i]]
+                val vertex2 = obj.vertexes[obj.indexes[i + 1]]
+                val vertex3 = obj.vertexes[obj.indexes[i + 2]]
+
+                g.drawLine(
+                    (vertex1.x * 100 + width / 2).toInt(),
+                    (-vertex1.y * 100 + height / 2).toInt(),
+                    (vertex2.x * 100 + width / 2).toInt(),
+                    (-vertex2.y * 100 + height / 2).toInt()
+                )
+
+                g.drawLine(
+                    (vertex2.x * 100 + width / 2).toInt(),
+                    (-vertex2.y * 100 + height / 2).toInt(),
+                    (vertex3.x * 100 + width / 2).toInt(),
+                    (-vertex3.y * 100 + height / 2).toInt()
+                )
+
+                g.drawLine(
+                    (vertex3.x * 100 + width / 2).toInt(),
+                    (-vertex3.y * 100 + height / 2).toInt(),
+                    (vertex1.x * 100 + width / 2).toInt(),
+                    (-vertex1.y * 100 + height / 2).toInt()
+                )
+            }
+        }
+    }
 }
