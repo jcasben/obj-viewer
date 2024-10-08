d874000c7d195c2d9c64353cff5c3c5b3ca9c867
jcasben <jcasben02@gmail.com>
2024-10-08 21:06:03 +0200

    [NEW]: implemented necessary DS

---
+++ src/core/structures/IndexedFace.kt
@@ -0,0 +1,6 @@
+package core.structures
+
+data class IndexedFace(
+    val vertexes: List<Vec4>,
+    val indexes: List<Int>
+)
---
+++ src/core/structures/Vec4.kt
@@ -0,0 +1,8 @@
+package core.structures
+
+data class Vec4(
+    var x: Float = 0f,
+    var y: Float = 0f,
+    var z: Float = 0f,
+    var w: Float = 0f
+)
