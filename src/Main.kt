3c489292bd669f6a741ded87464ebfdbe726e7c6
jcasben <jcasben02@gmail.com>
2024-10-08 21:07:06 +0200

    [UPDATE]: call to ObjViewer

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,24 @@
-fun main() {
+import ui.ObjectViewer
+import java.awt.Dimension
+import java.awt.event.WindowAdapter
+import java.awt.event.WindowEvent
+import javax.swing.JFrame
+import kotlin.system.exitProcess
 
+fun main() {
+    val frame = JFrame()
+    val panel = ObjectViewer()
+
+    panel.isVisible = true
+
+    frame.add(panel)
+    frame.size = Dimension(800, 600)
+    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+    frame.isVisible = true
+
+    frame.addWindowListener(object : WindowAdapter() {
+        override fun windowClosing(e: WindowEvent?) {
+            exitProcess(0)
+        }
+    })
 }
