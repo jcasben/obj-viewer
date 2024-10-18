862be6f0fc15defcb4b7cae35935629ab1c6d8fa
jcasben <jcasben02@gmail.com>
2024-10-18 12:35:48 +0200

    [FEAT]: created class that handles all events

---
+++ src/core/handlers/EventHandler.kt
@@ -0,0 +1,36 @@
+package core.handlers
+
+import core.models.ObjModel
+import ui.MainPanel
+import java.awt.event.ActionListener
+import java.io.File
+import javax.swing.JFileChooser
+import javax.swing.filechooser.FileNameExtensionFilter
+
+class EventHandler {
+    companion object {
+        fun loadObjectHandler(): ActionListener {
+            return ActionListener {
+                val objFilter = FileNameExtensionFilter("Obj Files", "obj")
+
+                val fileChooser = JFileChooser().apply {
+                    fileSelectionMode = JFileChooser.FILES_ONLY
+                    fileFilter = objFilter
+                    dialogTitle = "Select the obj file that you want to visualize"
+                    currentDirectory = File("./")
+                }
+
+                val result = fileChooser.showOpenDialog(null)
+
+                if (result != JFileChooser.CANCEL_OPTION) {
+                    val fileName: File = fileChooser.selectedFile
+                    if (fileName.name.isNotBlank()) {
+                        ObjModel.getInstance().path = fileName.name
+                        ObjModel.regenerateIndexedFace()
+                        MainPanel.drawObject()
+                    }
+                }
+            }
+        }
+    }
+}
