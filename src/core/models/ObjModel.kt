4dfbb7f1e3269ca136c8269029685c760af90ecf
jcasben <jcasben02@gmail.com>
2024-10-18 12:35:11 +0200

    [FEAT]: created ObjModel

---
+++ src/core/models/ObjModel.kt
@@ -0,0 +1,18 @@
+package core.models
+
+import core.utils.ObjLoader
+
+data class ObjModel(
+    var path: String = "bunny.obj",
+    var indexedFace: IndexedFace? = null,
+) {
+    companion object {
+        private val objModel = ObjModel()
+        fun getInstance(): ObjModel = objModel
+        fun regenerateIndexedFace() { objModel.indexedFace = ObjLoader.parseObj(objModel.path) }
+    }
+
+    init {
+        indexedFace = ObjLoader.parseObj(path)
+    }
+}
